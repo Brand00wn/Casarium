f4512a1c30752f8a139489d8d4fa4bc520af8d9c
Gustavo Brandão <sm70plus@hotmail.com>
2026-08-19 22:36:36 -0300

    fix: allow server actions on railway domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["bcryptjs"],
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["casarium-production.up.railway.app"],
+    },
+  },
 };
 
 export default nextConfig;
